e2a7bf01b11e15a25b0dc953bc60c71212a6c01a
Nicholas <na.stewart365@gmail.com>
2025-10-16 11:45:50 -0400

    Syntax revisions

--- index.js
+++ index.js
@@ -8,22 +8,18 @@ server.use(express.static('public'))
 
 function updateHitCounter(){
     const filePath = "hits.txt";
-    if(fs.existsSync(filePath)){
-        const data = fs.readFileSync(filePath, 'utf-8');
-        hits = parseInt(data);
-    }
+    if(fs.existsSync(filePath))
+        hits = parseInt(fs.readFileSync(filePath, 'utf-8'));
     hits++;
     fs.writeFileSync(filePath, hits.toString());
     return hits;
 }
+
 function getRandWord(){
     const filePath = "allwords.txt";
-    if(fs.existsSync(filePath)){
-        const data = fs.readFileSync(filePath, 'utf-8');
-        const lines = data.split('\n');
-        const randomLine = lines[Math.floor(Math.random() * lines.length)];
-
-        const [word, part, definition] = randomLine.split('\t');
+    if(fs.existsSync(filePath)) {
+        const lines = fs.readFileSync(filePath, 'utf-8').split('\n');
+        const [word, part, definition] = lines[Math.floor(Math.random() * lines.length)].split('\t');
         return {
             "word": word, 
             "part": part, 
@@ -33,14 +29,16 @@ function getRandWord(){
 }
 
 server.get('/hits', function(req, res){
-    const hits = updateHitCounter();
-    res.json({hitCount:hits});
+    res.json({
+        "hitCount": updateHitCounter()
+    });
 });
+
 server.get('/word', function(req, res){
-    const wordData = getRandWord();
-    res.json(wordData);
+    res.json(getRandWord());
 
 });
+
 server.listen(port, function (){
     console.log(`Listening on ${port}`)
 });
